8841be68693c0bdf78099abf8d0c61b80f7bf2c1
LSaldyt <lucassaldyt@gmail.com>
2022-05-02 15:24:41 -0700

    Misc

--- PathTracking/lqr_speed_steer_control/lqr_speed_steer_control.py
+++ PathTracking/lqr_speed_steer_control/lqr_speed_steer_control.py
@@ -5,6 +5,7 @@ Path tracking simulation with LQR speed and steering control
 author Atsushi Sakai (@Atsushi_twi)
 
 """
+
 import math
 import sys
 import os
@@ -34,13 +35,11 @@ show_animation = True
 
 
 class State:
-
     def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
         self.x = x
         self.y = y
         self.yaw = yaw
         self.v = v
-
 
 def update(state, a, delta):
 
