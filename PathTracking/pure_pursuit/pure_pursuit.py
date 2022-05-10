14fa9c31d433aa00cff0a923723ab51d04251e4c
LSaldyt <lucassaldyt@gmail.com>
2022-05-09 20:19:40 -0700

    Efficient jax code

--- PathTracking/pure_pursuit/pure_pursuit.py
+++ PathTracking/pure_pursuit/pure_pursuit.py
@@ -31,6 +31,7 @@ STEER_MAX  = np.deg2rad(57.5) # Yeah why not
 POSS_STEER = np.pi
 V_MAX      = 1.
 FORCE_MAX  = 1.
+ACCEL_FACT = 2.8
 PARAMS = np.array([b/BASE_MAX, m/MASS_MAX, STEER_MAX/POSS_STEER, FORCE_MAX])
 
 @jit
@@ -53,21 +54,25 @@ def dynamics(t, x, u):
                                   jnp.sin(yaw), jnp.cos(yaw), v))
     return next_state
 
+@jit
 def initial_state(x=0.0, y=0.0, yaw=0.0, v=0.0):
     rear_x = x - ((b / 2) * jnp.cos(yaw))
     rear_y = y - ((b / 2) * jnp.sin(yaw))
     return jnp.array([x, y, rear_x, rear_y, yaw, jnp.sin(yaw), jnp.cos(yaw), v, 0., 0.])
 
-def apply_noise(state, rng, state_noise):
-    state = state.at[:4].set(state[:4] + rng.normal(size=(4,), scale=state_noise))
+@jit
+def apply_noise(state, key, state_noise):
+    _, key = jr.split(key)
+    return state.at[:4].set(state[:4] + jr.normal(key, (4,)) * state_noise)
 
-# @partial(jit, static_argnums=0)
+@jit
 def update(state, f, delta, dt):
     f     = jnp.tanh(f) * FORCE_MAX
     delta = jnp.tanh(delta) * STEER_MAX
     state = state.at[-2:].set(jnp.array([delta, f]))
     x_new = dynamics(dt, state[:8], state[-2:])
     state = state.at[:8].set(x_new)
+    return state
 
 def calc_distance(state, point_x, point_y):
     dx = state[2] - point_x
@@ -76,7 +81,9 @@ def calc_distance(state, point_x, point_y):
 
 # @partial(jit, static_argnums=0)
 def append_state(states, i, state, ai, di):
+    state = state.at[-2:].set(jnp.array([di, ai]))
     states = states.at[i, :].set(state)
+    return states
 
 def vectorize(states, size=100.0):
     states_vec = np.asarray(states).copy()
@@ -85,8 +92,11 @@ def vectorize(states, size=100.0):
     states_vec[:, 5]  = np.sin(states_vec[:, 4])
     states_vec[:, 6]  = np.cos(states_vec[:, 5])
     states_vec[:, 7]  /= V_MAX
+    # states_vec[:, 8]  /= V_MAX
+    states_vec[:, 9]  /= ACCEL_FACT
     # jnp.array([jnp.tanh(di), jnp.tanh(ai)]))
     # assert np.max(np.abs(states_vec[:, :4])) < 1.5
+    print(np.max(np.abs(states_vec), axis=0))
     assert np.max(np.abs(states_vec[:, 4:])) < 1.00001
     return states_vec
 
@@ -142,7 +152,7 @@ def pure_pursuit_steer_control(state, cx, cy, old_i, pind):
     return delta, ind, old_i
 
 
-from numpy.random import default_rng
+import jax.random as jr
 
 def pure_pursuit(cx, cy, target_speed=10.0/3.6, x0=0, y0=--3.0, yaw0=0.0, v0=0.0,
         t_max=128.0, show_animation=False, size=100.0, dt=0.5,
@@ -152,35 +162,34 @@ def pure_pursuit(cx, cy, target_speed=10.0/3.6, x0=0, y0=--3.0, yaw0=0.0, v0=0.0
 
     # initial state
     state = initial_state(x=x0, y=y0, yaw=yaw0, v=v0)
-    rng = default_rng(seed)
+    key = jr.PRNGKey(seed)
+    _, key = jr.split(key)
     if state_noise is None:
-        state_noise = rng.uniform(low=0, high=2e-1) # Either no noise or max 2e-1
+        state_noise = jr.uniform(key, minval=0, maxval=2e-1) # Either no noise or max 2e-1
 
     lastIndex = len(cx) - 1
     time = 0.0
     i = 0
     i_max = int(t_max / dt)
     states = jnp.zeros((i_max, 10))
-    append_state(states, i, state, 0., 0.)
+    states = append_state(states, i, state, 0., 0.)
     target_ind, _, old_i = search_target_index(state, cx, cy, None)
 
     while t_max >= time and lastIndex > target_ind:
-        apply_noise(state, rng, state_noise)
+        state = apply_noise(state, key, state_noise)
         # Calc control input
         ai = proportional_control(target_speed, state[7])
-        ai += rng.normal(loc=0.0, scale=act_noise)
         try:
             di, target_ind, old_i = pure_pursuit_steer_control(
                 state, cx, cy, old_i, target_ind)
-            # di += rng.normal(loc=0.0, scale=act_noise)
         except IndexError: # If spline path is less than max time
             break
 
-        update(state, ai, di, dt)  # Control vehicle
+        state = update(state, ai, di, dt)  # Control vehicle
 
         time += dt
         i += 1
-        append_state(states, i, state, 0., 0.)
+        states = append_state(states, i, state, ai, di)
 
     print('Total time: ')
     print(time)
@@ -189,6 +198,10 @@ def pure_pursuit(cx, cy, target_speed=10.0/3.6, x0=0, y0=--3.0, yaw0=0.0, v0=0.0
     assert lastIndex >= target_ind, "Cannot goal"
 
     return vectorize(states)
+
+
+import plotly.express as px
+import plotly.graph_objects as go
 
 if __name__ == '__main__':
     print("Pure pursuit path tracking simulation start")
@@ -201,4 +214,7 @@ if __name__ == '__main__':
     traj = pure_pursuit(cx, cy, target_speed, show_animation=True, dt=0.5)
     print(traj.shape)
     print(traj)
-    1/0
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=cx/100., y=cy/100., name='spline'))
+    fig.add_trace(go.Scatter(x=traj[:, 0], y=traj[:, 1], name='pure pursuit'))
+    fig.show()
