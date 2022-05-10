f2be77e2338e57f1196622bdbd171e2f1bc77ef5
LSaldyt <lucassaldyt@gmail.com>
2022-05-09 19:55:49 -0700

    Completely refactor pure pursuit to be in "pure" jax :D

--- PathTracking/pure_pursuit/pure_pursuit.py
+++ PathTracking/pure_pursuit/pure_pursuit.py
@@ -12,6 +12,8 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from functools import partial
+from rich.traceback import install
+install(show_locals=False)
 
 class FinishError(Exception):
     pass
@@ -51,145 +53,105 @@ def dynamics(t, x, u):
                                   jnp.sin(yaw), jnp.cos(yaw), v))
     return next_state
 
-class State:
-    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
-        self.x = x
-        self.y = y
-        self.yaw = yaw
-        self.v = v
-        self.rear_x = self.x - ((b / 2) * math.cos(self.yaw))
-        self.rear_y = self.y - ((b / 2) * math.sin(self.yaw))
-        self.x_array = jnp.array([self.x, self.y, self.rear_x, self.rear_y,
-            self.yaw, jnp.sin(self.yaw), jnp.cos(self.yaw), self.v])
-        self.u_array = jnp.array([0., 0.]) # Whatever :)
+def initial_state(x=0.0, y=0.0, yaw=0.0, v=0.0):
+    rear_x = x - ((b / 2) * jnp.cos(yaw))
+    rear_y = y - ((b / 2) * jnp.sin(yaw))
+    return jnp.array([x, y, rear_x, rear_y, yaw, jnp.sin(yaw), jnp.cos(yaw), v, 0., 0.])
 
-    def apply_noise(self, rng, state_noise):
-        self.x_array = self.x_array.at[:4].set(self.x_array[:4]
-                + rng.normal(size=(4,), scale=state_noise))
+def apply_noise(state, rng, state_noise):
+    state = state.at[:4].set(state[:4] + rng.normal(size=(4,), scale=state_noise))
 
-    # @partial(jit, static_argnums=0)
-    def update(self, f, delta, dt):
-        f     = jnp.tanh(f) * FORCE_MAX
-        delta = jnp.tanh(delta) * STEER_MAX
-        self.u_array = self.u_array.at[:].set(jnp.array([delta, f]))
-        x_new = dynamics(dt, self.x_array, self.u_array)
-        self.x_array = self.x_array.at[:].set(x_new)
+# @partial(jit, static_argnums=0)
+def update(state, f, delta, dt):
+    f     = jnp.tanh(f) * FORCE_MAX
+    delta = jnp.tanh(delta) * STEER_MAX
+    state = state.at[-2:].set(jnp.array([delta, f]))
+    x_new = dynamics(dt, state[:8], state[-2:])
+    state = state.at[:8].set(x_new)
 
-    def calc_distance(self, point_x, point_y):
-        dx = self.rear_x - point_x
-        dy = self.rear_y - point_y
-        return math.hypot(dx, dy)
+def calc_distance(state, point_x, point_y):
+    dx = state[2] - point_x
+    dy = state[3] - point_y
+    return jnp.hypot(dx, dy)
 
-class States:
-    def __init__(self, size):
-        self.states = jnp.zeros(size)
+# @partial(jit, static_argnums=0)
+def append_state(states, i, state, ai, di):
+    states = states.at[i, :].set(state)
 
-    # @partial(jit, static_argnums=0)
-    def append(self, t, i, state, ai, di):
-        self.states = self.states.at[i, :-2].set(state.x_array)
-        self.states = self.states.at[i, -2:].set(
-                jnp.array([jnp.tanh(di), jnp.tanh(ai)]))
-
-    def vectorize(self, size=100.0):
-        states_vec = np.asarray(self.states).copy()
-        states_vec[:, :4] /= size
-        states_vec[:, 3]  /= POSS_STEER
-        states_vec[:, 4]  = np.sin(states_vec[:, 4])
-        states_vec[:, 5]  = np.cos(states_vec[:, 5])
-        states_vec[:, 6]  /= V_MAX
-        # assert np.max(np.abs(states_vec[:, :4])) < 1.5
-        assert np.max(np.abs(states_vec[:, 4:])) < 1.00001
-        print(states_vec.shape)
-        return states_vec
+def vectorize(states, size=100.0):
+    states_vec = np.asarray(states).copy()
+    states_vec[:, :4] /= size
+    states_vec[:, 4]  /= POSS_STEER
+    states_vec[:, 5]  = np.sin(states_vec[:, 4])
+    states_vec[:, 6]  = np.cos(states_vec[:, 5])
+    states_vec[:, 7]  /= V_MAX
+    # jnp.array([jnp.tanh(di), jnp.tanh(ai)]))
+    # assert np.max(np.abs(states_vec[:, :4])) < 1.5
+    assert np.max(np.abs(states_vec[:, 4:])) < 1.00001
+    return states_vec
 
 def proportional_control(target, current):
     a = Kp * (target - current)
     return a
 
-class TargetCourse:
-    def __init__(self, cx, cy):
-        self.cx = cx
-        self.cy = cy
-        self.old_nearest_point_index = None
+def search_target_index(state, cx, cy, old_i):
+    if old_i is None:
+        # search nearest point index
+        d = jnp.hypot(state[2] - cx, state[3] - cy)
+        ind = jnp.argmin(d)
+        old_i = ind
+    else:
+        ind = old_i
+        distance_this_index = calc_distance(state, cx[ind], cy[ind])
+        while True:
+            distance_next_index = calc_distance(state, cx[ind + 1], cy[ind + 1])
+            if distance_this_index < distance_next_index:
+                break
+            ind = ind + 1 if (ind + 1) < len(cx) else ind
+            distance_this_index = distance_next_index
+        old_i = ind
 
-    def search_target_index(self, state):
+    Lf = k * state[7] + Lfc  # update look ahead distance
 
-        # To speed up nearest point search, doing it at only first time.
-        if self.old_nearest_point_index is None:
-            # search nearest point index
-            dx = [state.rear_x - icx for icx in self.cx]
-            dy = [state.rear_y - icy for icy in self.cy]
-            d = np.hypot(dx, dy)
-            ind = np.argmin(d)
-            self.old_nearest_point_index = ind
-        else:
-            ind = self.old_nearest_point_index
-            distance_this_index = state.calc_distance(self.cx[ind],
-                                                      self.cy[ind])
-            while True:
-                distance_next_index = state.calc_distance(self.cx[ind + 1],
-                                                          self.cy[ind + 1])
-                if distance_this_index < distance_next_index:
-                    break
-                ind = ind + 1 if (ind + 1) < len(self.cx) else ind
-                distance_this_index = distance_next_index
-            self.old_nearest_point_index = ind
+    # search look ahead target point index
+    while Lf > calc_distance(state, cx[ind], cy[ind]):
+        if (ind + 1) >= len(cx):
+            break  # not exceed goal
+        ind += 1
 
-        Lf = k * state.v + Lfc  # update look ahead distance
-
-        # search look ahead target point index
-        while Lf > state.calc_distance(self.cx[ind], self.cy[ind]):
-            if (ind + 1) >= len(self.cx):
-                break  # not exceed goal
-            ind += 1
-
-        return ind, Lf
+    return ind, Lf, old_i
 
 
-def pure_pursuit_steer_control(state, trajectory, pind):
-    ind, Lf = trajectory.search_target_index(state)
+def pure_pursuit_steer_control(state, cx, cy, old_i, pind):
+    ind, Lf, old_i = search_target_index(state, cx, cy, old_i)
 
     if pind >= ind:
         ind = pind
 
-    if ind < len(trajectory.cx):
-        tx = trajectory.cx[ind]
-        ty = trajectory.cy[ind]
+    if ind < len(cx):
+        tx = cx[ind]
+        ty = cy[ind]
     else:  # toward goal
-        tx = trajectory.cx[-1]
-        ty = trajectory.cy[-1]
-        ind = len(trajectory.cx) - 1
+        tx = cx[-1]
+        ty = cy[-1]
+        ind = len(cx) - 1
 
-    alpha = math.atan2(ty - state.rear_y, tx - state.rear_x) - state.yaw
-    delta = math.atan2(2.0 * b * math.sin(alpha) / Lf, 1.0)
+    alpha = jnp.arctan2(ty - state[3], tx - state[2]) - state[4]
+    delta = jnp.arctan2(2.0 * b * math.sin(alpha) / Lf, 1.0)
 
-    return delta, ind
+    return delta, ind, old_i
 
-
-def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
-    """
-    Plot arrow
-    """
-
-    if not isinstance(x, float):
-        for ix, iy, iyaw in zip(x, y, yaw):
-            plot_arrow(ix, iy, iyaw)
-    else:
-        plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
-                  fc=fc, ec=ec, head_width=width, head_length=width)
-        plt.plot(x, y)
 
 from numpy.random import default_rng
 
 def pure_pursuit(cx, cy, target_speed=10.0/3.6, x0=0, y0=--3.0, yaw0=0.0, v0=0.0,
         t_max=128.0, show_animation=False, size=100.0, dt=0.5,
-        # state_noise=5e-2,
         state_noise=None,
         act_noise=0.,
         seed=2022):
 
     # initial state
-    state = State(x=x0, y=y0, yaw=yaw0, v=v0)
+    state = initial_state(x=x0, y=y0, yaw=yaw0, v=v0)
     rng = default_rng(seed)
     if state_noise is None:
         state_noise = rng.uniform(low=0, high=2e-1) # Either no noise or max 2e-1
@@ -198,49 +160,27 @@ def pure_pursuit(cx, cy, target_speed=10.0/3.6, x0=0, y0=--3.0, yaw0=0.0, v0=0.0
     time = 0.0
     i = 0
     i_max = int(t_max / dt)
-    states = States((i_max, 10))
-    states.append(time, i, state, 0., 0.)
-    target_course = TargetCourse(cx, cy)
-    target_ind, _ = target_course.search_target_index(state)
+    states = jnp.zeros((i_max, 10))
+    append_state(states, i, state, 0., 0.)
+    target_ind, _, old_i = search_target_index(state, cx, cy, None)
 
     while t_max >= time and lastIndex > target_ind:
-        state.apply_noise(rng, state_noise)
+        apply_noise(state, rng, state_noise)
         # Calc control input
-        ai = proportional_control(target_speed, state.v)
+        ai = proportional_control(target_speed, state[7])
         ai += rng.normal(loc=0.0, scale=act_noise)
         try:
-            di, target_ind = pure_pursuit_steer_control(
-                state, target_course, target_ind)
-            di += rng.normal(loc=0.0, scale=act_noise)
+            di, target_ind, old_i = pure_pursuit_steer_control(
+                state, cx, cy, old_i, target_ind)
+            # di += rng.normal(loc=0.0, scale=act_noise)
         except IndexError: # If spline path is less than max time
             break
 
-        state.update(ai, di, dt)  # Control vehicle
-        if abs(state.x) > size or abs(state.y) > size:
-            break # Out of bounds
+        update(state, ai, di, dt)  # Control vehicle
 
         time += dt
         i += 1
-        states.append(time, i, state, ai, di)
-
-        # if show_animation:  # pragma: no cover
-        #     plt.cla()
-        #     # for stopping simulation with the esc key.
-        #     def callback(event):
-        #         if event.key == 'escape':
-        #             return states.vectorize
-        #             exit(0)
-        #         else:
-        #             return [None]
-        #     plt.gcf().canvas.mpl_connect('key_release_event', callback)
-        #     plot_arrow(state.x, state.y, state.yaw)
-        #     plt.plot(cx, cy, "-r", label="course")
-        #     plt.plot(states.x, states.y, "-b", label="trajectory")
-        #     plt.plot(cx[target_ind], cy[target_ind], "xg", label="target")
-        #     plt.axis("equal")
-        #     plt.grid(True)
-        #     plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
-        #     plt.pause(0.001)
+        append_state(states, i, state, 0., 0.)
 
     print('Total time: ')
     print(time)
@@ -248,34 +188,13 @@ def pure_pursuit(cx, cy, target_speed=10.0/3.6, x0=0, y0=--3.0, yaw0=0.0, v0=0.0
     # Test
     assert lastIndex >= target_ind, "Cannot goal"
 
-    try:
-        pass
-        # if show_animation:  # pragma: no cover
-        #     plt.cla()
-        #     plt.plot(cx, cy, ".r", label="course")
-        #     plt.plot(states.x, states.y, "-b", label="trajectory")
-        #     plt.legend()
-        #     plt.xlabel("x[m]")
-        #     plt.ylabel("y[m]")
-        #     plt.axis("equal")
-        #     plt.grid(True)
-
-        #     plt.subplots(1)
-        #     plt.plot(states.t, [iv * 3.6 for iv in states.v], "-r")
-        #     plt.xlabel("Time[s]")
-        #     plt.ylabel("Speed[km/h]")
-        #     plt.grid(True)
-        #     plt.show()
-    except KeyboardInterrupt:
-        pass
-
-    return states.vectorize(size)
+    return vectorize(states)
 
 if __name__ == '__main__':
     print("Pure pursuit path tracking simulation start")
     #  target course
     cx = np.arange(0, 50, 0.5)
-    cy = [math.sin(ix / 5.0) * ix / 2.0 for ix in cx]
+    cy = np.array([math.sin(ix / 5.0) * ix / 2.0 for ix in cx])
 
     target_speed = 10.0 / 3.6  # [m/s]
 
